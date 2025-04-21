e7794fe801333ea403f0108cb82ae26f176848e8
fledxy <trinhthienbao2003@gmail.com>
2025-04-21 22:30:59 +0700

    try to run terraform

--- app.tf/backend.tf
+++ app.tf/backend.tf
@@ -1,7 +1,7 @@
-terraform {
-  backend "s3" {
-    bucket = "fledxytfbucket"
-    key    = "dev-tf.tfstate"
-    region = "ap-southeast-1"
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket = "fledxytfbucket"
+#     key    = "dev-tf.tfstate"
+#     region = "ap-southeast-1"
+#   }
+# }
