d72f0f6a7629a1156072197f91519faa42d76780
fledxy <trinhthienbao2003@gmail.com>
2025-05-28 16:29:37 +0700

    Deployment application

--- benefits.dev/frontend/src/config.ts
+++ benefits.dev/frontend/src/config.ts
@@ -1,5 +1,5 @@
 const config = {
-    apiUrl: process.env.REACT_APP_API_URL || 'http://127.0.0.1:5000/api'
+    apiUrl: process.env.REACT_APP_API_URL || 'http://127.0.0.1:5000/api/logs'
 };
 
 export default config; 
