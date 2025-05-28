0ab549c403deaa72e0cef8f3e8206d6c6a93c98c
fledxy <trinhthienbao2003@gmail.com>
2025-05-28 16:46:22 +0700

    Deployment application

--- app.tf/_modules/eks/main.tf
+++ app.tf/_modules/eks/main.tf
@@ -31,7 +31,7 @@ resource "aws_eks_cluster" "eks" {
   ]
 }
 resource "aws_eks_node_group" "eks-node-group" {
-  cluster_name    = var.cluster_name
+  cluster_name    = "${var.cluster_name}-eks"
   node_group_name = "${var.cluster_name}-default-node-group"
   node_role_arn   = aws_iam_role.node.arn
   subnet_ids      = var.private_subnet_ids
