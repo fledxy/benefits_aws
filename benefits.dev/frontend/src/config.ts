ecd5977285e4bd0e670199301967ecfaa9206768
fledxy <trinhthienbao2003@gmail.com>
2025-05-28 22:14:11 +0700

    Fix Frontend URL Connect

--- benefits.dev/frontend/src/config.ts
+++ benefits.dev/frontend/src/config.ts
@@ -1,5 +1,5 @@
 const config = {
-    apiUrl: process.env.REACT_APP_API_URL || 'http://127.0.0.1:5000/api/logs'
+    apiUrl: process.env.REACT_APP_API_URL || '/api/logs'
 };
 
 export default config; 
